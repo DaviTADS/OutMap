e2d79464f9d7bc3b835792e40ebf036d2726e4b8
david <davidxpro16@hotmail.com>
2019-10-03 21:55:27 -0300

    first

---
+++ app/src/main/java/br/edu/ifpe/tads/pdm/pratica05/MapsActivity.java
@@ -0,0 +1,185 @@
+package br.edu.ifpe.tads.pdm.pratica05;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Date;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private static int FINE_LOCATION_REQUEST = 0;
+    private boolean fine_location;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        requestPermission();
+
+        //createToolbar();
+    }
+
+    private void createToolbar(){
+
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_drawer);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+         //setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+       // navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void requestPermission() {
+
+        int permissioncheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        boolean hasPermission = (permissioncheck == PackageManager.PERMISSION_GRANTED);
+
+        if (hasPermission) return;
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                FINE_LOCATION_REQUEST);
+
+    }
+
+//    public void onRequestPermissionsResult(int requestCode,
+//                                           String permissions[], int[] grantResults) {
+//        boolean granted = (grantResults.length > 0) &&
+//                (grantResults[0] == PackageManager.PERMISSION_GRANTED);
+//        fine_location = (requestCode == FINE_LOCATION_REQUEST) && granted;
+//
+//        findViewById(R.id.button_location).setEnabled(fine_location);
+//    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        LatLng recife = new LatLng(-8.05, -34.9);
+        LatLng caruaru = new LatLng(-8.27, -35.98);
+        LatLng joaopessoa = new LatLng(-7.12, -34.84);
+
+        mMap.addMarker(new MarkerOptions().
+                position(recife).
+                title("Recife").
+                icon(BitmapDescriptorFactory.defaultMarker(35)));
+        mMap.addMarker(new MarkerOptions().
+                position(caruaru).
+                title("Caruaru").
+                icon(BitmapDescriptorFactory.defaultMarker(120)));
+        mMap.addMarker(new MarkerOptions().
+                position(joaopessoa).
+                title("João Pessoa").
+                icon(BitmapDescriptorFactory.defaultMarker(230)));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(recife));
+
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(MapsActivity.this,
+                        "Você clicou em " + marker.getTitle(),
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap.addMarker(new MarkerOptions().
+                        position(latLng).
+                        title("Adicionado em " + new Date()).
+                        icon(BitmapDescriptorFactory.defaultMarker(0)));
+            }
+        });
+
+
+    }
+
+    public void currentLocation(View view) {
+        FusedLocationProviderClient fusedLocationProviderClient =
+                LocationServices.getFusedLocationProviderClient(this);
+
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location!=null) {
+                    Toast.makeText(MapsActivity.this, "Localização atual: Latitude = " +
+                            location.getLatitude() + " Longitude = " +
+                            location.getLongitude(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
